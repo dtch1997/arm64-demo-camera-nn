b0234ada440c5baaee3466a0a74e42658eeae710
Daniel <dtch009@gmail.com>
2020-06-17 09:06:05 +0000

    Move constructor to header file

--- BSP_Camera.h
+++ BSP_Camera.h
@@ -1,12 +1,15 @@
 # ifndef BSP_CAMERA_H
 # define BSP_CAMERA_H
 
-#include "Camera.h"
+# include "Camera.h"
 
 class BSP_Camera : public Camera<uint8_t>
 {
     public:
-        BSP_Camera(image_size_t sz, uint32_t resolution);
+        BSP_Camera(image_size_t sz, uint32_t resolution): Camera<uint8_t>(sz) 
+        {
+            this->resolution_ = resolution;
+        };
         void PowerOn(void);
         void PowerOff(void);
         void Initialize(void);
