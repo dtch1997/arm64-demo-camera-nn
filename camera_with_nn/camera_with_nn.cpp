398858269bfd30440e9b3a95df755ee963edc48b
Daniel <dtch009@gmail.com>
2020-06-19 03:56:44 +0000

    Added nn functionality

--- camera_with_nn/camera_with_nn.cpp
+++ camera_with_nn/camera_with_nn.cpp
@@ -2,6 +2,18 @@
 #include "stm32746g_discovery_camera.h"
 #include "LCD_DISCO_F746NG.h"
 #include "stm32f7xx_hal_dcmi.h"
+#include "nn.h"
+
+extern "C" {
+  //defined in stm32746g_discovery_camera.c
+  extern DCMI_HandleTypeDef hDcmiHandler;
+  void DCMI_IRQHandler(void) {
+      HAL_DCMI_IRQHandler(&hDcmiHandler);
+  }
+  void DMA2_Stream1_IRQHandler(void) {
+      HAL_DMA_IRQHandler(hDcmiHandler.DMA_Handle);
+  }
+}
 
 // 2 input channels, as input is in RGB565 format
 #define NUM_IN_CH 2
@@ -16,7 +28,7 @@ uint8_t resized_buffer[NUM_OUT_CH*CNN_IMG_SIZE*CNN_IMG_SIZE];
 char lcd_output_string[50];
 LCD_DISCO_F746NG lcd;
 Serial pc(USBTX, USBRX);
-
+ 
 void resize_rgb565in_rgb888out(uint8_t* camera_image, uint8_t* resize_image)
 {
   // offset so that only the center part of rectangular image is selected for resizing
@@ -73,7 +85,22 @@ void display_image_rgb565(int x_dim, int y_dim, uint8_t* image_data)
     }
   }
 }
- 
+
+const char* cifar10_label[] = {"Plane", "Car", "Bird", "Cat", "Deer", "Dog", "Frog", "Horse", "Ship", "Truck"};
+q7_t output_data[10]; //10-classes
+int get_top_prediction(q7_t* predictions)
+{
+  int max_ind = 0;
+  int max_val = -128;
+  for(int i=0;i<10;i++) {
+    if(max_val < predictions[i]) {
+      max_val = predictions[i];
+      max_ind = i;
+    }
+  }
+  return max_ind;
+}
+
 int main()
 {
   pc.baud(115200);
@@ -88,14 +115,22 @@ int main()
   }
   wait_ms(100);
 
-
   while(1) {
     BSP_CAMERA_SnapshotStart(camera_buffer);
     resize_rgb565in_rgb888out(camera_buffer, resized_buffer);
     display_image_rgb888(CNN_IMG_SIZE, CNN_IMG_SIZE, resized_buffer);
     display_image_rgb565(IMG_WIDTH, IMG_HEIGHT, camera_buffer);
     // run neural network 
-    sprintf(lcd_output_string,"Original vs. scaled images");
-    lcd.DisplayStringAt(0, LINE(8), (uint8_t *)lcd_output_string, CENTER_MODE);
+    run_nn((q7_t*)resized_buffer, output_data);
+    // Softmax: to get predictions
+    arm_softmax_q7(output_data,IP1_OUT_DIM,output_data);
+    int top_ind = get_top_prediction(output_data);
+    sprintf(lcd_output_string,"  Prediction: %s       ",cifar10_label[top_ind]);
+    lcd.DisplayStringAt(0, LINE(8), (uint8_t *)lcd_output_string, LEFT_MODE);
+    sprintf(lcd_output_string,"  Confidence: %.1f%%   ",(output_data[top_ind]/127.0)*100.0);
+    lcd.DisplayStringAt(0, LINE(9), (uint8_t *)lcd_output_string, LEFT_MODE);
   }
+
 }
+
+
